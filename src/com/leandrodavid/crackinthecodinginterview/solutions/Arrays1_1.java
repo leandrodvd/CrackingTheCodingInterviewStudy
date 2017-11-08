840c6f8716a25cab3d2d688927310a5e80163c65
ldavid <leandrodvd@gmail.com>
2017-11-07 22:36:42 -0200

    first commit

---
+++ src/com/leandrodavid/crackinthecodinginterview/solutions/Arrays1_1.java
@@ -0,0 +1,10 @@
+package com.leandrodavid.crackinthecodinginterview.solutions;
+
+public class Arrays1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
